63e9ed45cde28390558134394bcd194d729e3c5a
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2026-02-02 14:01:33 -0700

    Update main with ski resort list and refactor to use skiresortinfo class.

--- Program.cs
+++ Program.cs
@@ -10,39 +10,16 @@ namespace SnowTracker
     {
         static void Main(string[] args)
         {
-            // Use Selenum driver to fully load page content (Create class)
-            var options = new ChromeOptions();
-            options.AddArgument("--headless"); // run without opening browser
-            options.AddArgument("--disable-gpu");
-            options.AddArgument("--no-sandbox");
-
-            using var driver = new ChromeDriver(options);
-            String url = "https://www.snow-forecast.com/resorts/Lake-Louise/6day/mid";
-            driver.Navigate().GoToUrl(url);
-            System.Threading.Thread.Sleep(5000); // wait to load JS content
-            string pageSource = driver.PageSource;
-
-            // send get request to loaded page source
-            
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(pageSource);
-
-            // get the latest snowfall
-            var newSnowfallElement = htmlDocument.DocumentNode.SelectSingleNode("//div[contains(@class,'about-weather-summary__snow-information-value')]/span[not(@class)]");
-            
-            if (newSnowfallElement == null)
+            List<string> skiResorts = new List<string>
             {
-                Console.WriteLine("Snowfall element NOT found.");
-                return;
+                "Lake-Louise",
+                "Sunshine"
+            };
+            foreach(string resort in skiResorts)
+            {
+                Console.WriteLine(resort);
+                SkiResortInfo.GetNewSnowfall(resort);
             }
-            
-            string newSnowfall = newSnowfallElement.InnerText.Trim();
-            Console.WriteLine("New Snowfall: " + newSnowfall);
-
-            // get the location
-
-
-            driver.Quit();
         }
     }
 }
