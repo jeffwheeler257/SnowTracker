369fd9cfebd32b7dc8c3e29da20773475da5f700
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2026-02-03 08:28:42 -0700

    Minor updates.

--- Program.cs
+++ Program.cs
@@ -12,13 +12,20 @@ namespace SnowTracker
         {
             List<string> skiResorts = new List<string>
             {
-                "Lake-Louise",
-                "Sunshine"
+                // "Lake-Louise",
+                // "Sunshine",
+                "Fernie"
             };
             foreach(string resort in skiResorts)
             {
                 Console.WriteLine(resort);
                 Console.WriteLine("Snowfall: " + SkiResortInfo.GetNewSnowfall(resort));
+                Console.WriteLine("Forecast raw data:");
+                int[] forecast = SkiResortInfo.getSnowForecast(resort);
+                foreach (int data in forecast)
+                {
+                    Console.WriteLine(data + " cm");
+                }
             }
         }
     }
