c2f1584f202ccef4885da7fdbc5e43ea53653296
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2026-02-03 15:29:42 -0700

    Create method to send email containing daily snow report table.

--- EmailService.cs
+++ EmailService.cs
@@ -5,20 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.DevTools.V142.CSS;
 using OpenQA.Selenium.DevTools.V142.Storage;
+using System.Net;
+using System.Net.Mail;
+using dotenv.net;
+
 
 namespace SnowTracker
 {
     public class EmailService
     {
-        private List<string> emailList = new List<string>
+        public static void SendEmail(List<SkiResortInfo> resortInfoList)
         {
-            "jeffwheeler257@gmail.com"
-        };
-
-        // public void SendDailyEmail()
-        // {
+            DotEnv.Load();
+            string? senderEmail = Environment.GetEnvironmentVariable("SENDER_EMAIL");
+            string? senderEmailPassword = Environment.GetEnvironmentVariable("SENDER_PASSWORD");
             
-        // }
+            if (senderEmail == null || senderEmailPassword == null)
+            {
+                Console.WriteLine("No sender email given");
+                return;
+            }
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Port = 587;
+            smtpClient.Credentials = new NetworkCredential(senderEmail, senderEmailPassword);
+            smtpClient.EnableSsl = true;
+
+            MailMessage email = new MailMessage();
+            email.From = new MailAddress(senderEmail);
+            email.Subject = "Daily Snow Reports";
+            email.Body = GenerateEmailContent(resortInfoList);
+            email.IsBodyHtml = true;
+
+            string? distributionList = Environment.GetEnvironmentVariable("MAIL_DISTRIBUTION_LIST");
+
+            if (distributionList == null)
+            {
+                Console.WriteLine("No email distribution list");
+                return;
+            }
+
+            string[] recipients = distributionList.Split(
+                ',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+            );
+
+            foreach (string recipient in recipients)
+            {
+                email.To.Add(recipient);
+            }
+
+            try
+            {
+                smtpClient.Send(email);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine("Mail sent succesfully.");
+
+        }
 
         public static string GenerateEmailContent(List<SkiResortInfo> resortInfoList)
         {
