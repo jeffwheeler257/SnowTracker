8a2c6056ccd7bf5629b8e88aebfe557f5d11a3c9
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2026-02-03 14:24:17 -0700

    Add method to generate html table content for daily email.

--- EmailService.cs
+++ EmailService.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.DevTools.V142.CSS;
+using OpenQA.Selenium.DevTools.V142.Storage;
 
 namespace SnowTracker
 {
@@ -17,9 +20,60 @@ namespace SnowTracker
             
         // }
 
-        // public string GenerateEmailContent()
-        // {
+        public static string GenerateEmailContent(List<SkiResortInfo> resortInfoList)
+        {
+            StringBuilder sb = new StringBuilder();
+            var props = typeof(SkiResortInfo).GetProperties();
+
+            sb.Append("""
+                <html>
+                <body style="font-family: Arial, sans-serif;">
+                <h3>Daily Snow Report</h3>
+                <table border="1" cellpadding="6" cellspacing="0" style="border-collapse:collapse;">
+                    <tr style="background:#f2f2f2;">
+                        <th>Resort</th>
+                        <th>New Snow (cm)</th>
+                        <th>Top Depth (cm)</th>
+                        <th>Bottom Depth (cm)</th>
+                        <th>+1 day Forecast (cm)</th>
+                        <th>+2 day Forecast (cm)</th>
+                        <th>+3 day Forecast (cm)</th>
+                        <th>+4 day Forecast (cm)</th>
+                        <th>+5 day Forecast (cm)</th>
+                        <th>+6 day Forecast (cm)</th>
+                        <th>Weather Overview</th>
+                    </tr>
+            """);
+
+            foreach(SkiResortInfo resort in resortInfoList)
+            {
+                sb.Append("<tr>");
+                foreach(var prop in props)
+                {
+                    var value = prop.GetValue(resort);
+                    if (value is int[] forecasts)
+                    {
+                        foreach (int forecast in forecasts)
+                        {
+                            sb.Append($"<td>{forecast}</td>");
+                        }
+                        
+                    }else
+                    {
+                        sb.Append($"<td>{value}</td>");
+                    }
+                    
+                }
+                sb.Append("</tr>");
+            }
+
+            sb.Append("""
+                </table>
+                </body>
+                </html>
+            """);
             
-        // }
+            return sb.ToString();
+        }
     }
 }
