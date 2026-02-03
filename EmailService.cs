22f725c08adf230514a52017f652ceed047fd1f9
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2026-02-03 08:27:50 -0700

    Create class to generate and send emails.

---
+++ EmailService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnowTracker
+{
+    public class EmailService
+    {
+        private List<string> emailList = new List<string>
+        {
+            "jeffwheeler257@gmail.com"
+        };
+
+        public void SendDailyEmail()
+        {
+            
+        }
+
+        public string GenerateEmailContent()
+        {
+            
+        }
+    }
+}
